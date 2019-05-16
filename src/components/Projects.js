8f925468eafa3f8ab0feec86443dcc9794cd230f
Jessica Poe <jesspoe00@gmail.com>
2019-05-16 14:38:49 -0700

    added home page with font awesome icons and a background

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class Projects extends Component {
   render() {
     return (
-      <div>
-        Here I am !
+      <div class="wrapper">
+        <header class="header">My header</header>
+
+        <div class="panel">Project 1</div>
+        <div class="panel">Project 1</div>
+        <div class="panel">Project 1</div>
+        <div class="panel">Project 1</div>
+
+
+        <footer class="footer">
+          <p>jesspoe00@gmail.com </p>
+          <p>(321) 863-5375</p>
+
+        </footer>
       </div>
     );
   }
