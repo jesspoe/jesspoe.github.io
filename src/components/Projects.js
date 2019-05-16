c53708586a5660c155b5d7a0dd4084e4f0fe1cdd
Jessica Poe <jesspoe00@gmail.com>
2019-05-16 16:40:07 -0700

    added images from my projects and styled with css grid

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import Campy from '../image/campy.png'
+import Guestbook from '../image/guestbook.png'
+import Pet from '../image/pet.png'
+import Poems from '../image/poems.png'
+import Veg from '../image/veg.png'
+
 
 class Projects extends Component {
   render() {
     return (
       <div class="wrapper">
-        <header class="header">My header</header>
+        <header class="header">
+          <Link title="Home" to="/">Home
+     </Link></header>
 
-        <div class="panel">Project 1</div>
-        <div class="panel">Project 1</div>
-        <div class="panel">Project 1</div>
-        <div class="panel">Project 1</div>
+        <div class="panel a">
+          <img src={Poems} alt="Poems" /></div>
+
+
+        <div class="panel b">
+          <img src={Veg} alt="Veg" /></div>
+
+
+        <div class="panel c">
+          <img src={Guestbook} alt="guestbook" /></div>
+
+        <div class="panel d">
+          <img src={Campy} alt="Campy" />
+
+        </div>
+
+
+        <div class="panel e">
+          <img src={Pet} alt="Pet" /></div>
 
 
         <footer class="footer">
