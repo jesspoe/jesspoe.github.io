14f6904607b2059a55b6f86d288d0c0447096750
Jessica Poe <jesspoe00@gmail.com>
2019-05-20 16:42:21 -0700

    Updates

---
+++ precache-manifest.4122efdf844310aa35391c36436d8df2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25106f1a2f6dc8016dbd8651ca5d1c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aeec3630dc4b71786cb6",
+    "url": "/static/css/main.9525a6ee.chunk.css"
+  },
+  {
+    "revision": "cf47f36172d3f1b874e9",
+    "url": "/static/js/2.685dc88d.chunk.js"
+  },
+  {
+    "revision": "aeec3630dc4b71786cb6",
+    "url": "/static/js/main.ee75fe31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c97cfc60fcdc0ad5101ceda2ff1a8fe5",
+    "url": "/static/media/Resume.c97cfc60.pdf"
+  },
+  {
+    "revision": "1aa51377d1862437661cd85bdb8c8cf1",
+    "url": "/static/media/background3.1aa51377.jpg"
+  },
+  {
+    "revision": "a99eab80676d5cc35f49b38ce1010a15",
+    "url": "/static/media/campy.a99eab80.png"
+  },
+  {
+    "revision": "6cef212001ef14de27fcd769bdd16cb2",
+    "url": "/static/media/guestbook.6cef2120.png"
+  },
+  {
+    "revision": "6c883e65ab34d72e2e183db4884ab073",
+    "url": "/static/media/headshot2.6c883e65.jpg"
+  },
+  {
+    "revision": "1f634b59dc630162e90ad6223d4e851a",
+    "url": "/static/media/pet.1f634b59.png"
+  },
+  {
+    "revision": "c7ba253daa0d5736523a5cb90afded30",
+    "url": "/static/media/poems.c7ba253d.png"
+  },
+  {
+    "revision": "4292cba7d974c26df6da4a054730fc38",
+    "url": "/static/media/veg.4292cba7.png"
+  }
+]);
